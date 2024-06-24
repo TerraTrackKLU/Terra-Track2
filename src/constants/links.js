1fdb2b05e702e0d1acd41433003f40ef46f721c3
arigmehmetsait <1200505043@ogrenci.kirklareli.edu.tr>
2024-06-24 23:01:37 +0300

    Post Id Çekme

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,8 +1,10 @@
-const MY_IP = "192.168.1.34";
+const MY_IP = "172.20.208.1";
 
 export const BASE_URL = `http://${MY_IP}:5000/terra-track/api`;
 export const REGISTER = BASE_URL + "/auth/register";
 export const LOGIN = BASE_URL + "/auth/login";
 export const GET_USER = BASE_URL + "/auth/get-user";
+export const UPDATE_USER = BASE_URL + "/auth/update-user";
 export const POST_SHARE = BASE_URL + "/posts/create";
 export const POST_HOMEPAGE = BASE_URL + "/posts";
+export const PROFILE = BASE_URL + "/posts";
