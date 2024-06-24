7cf4dd1b8248a1e68e73831c20d23ae0c9e37582
augurgaug <ugurgl_65@hotmail.com>
2024-06-24 17:27:18 +0300

    myposts

---
+++ src/screens/MyPosts.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Image } from "react-native";
+import axios from "axios";
+import { Appbar, Avatar, Card, Button as PaperButton } from "react-native-paper";
+import { useSelector } from "react-redux";
+import { POST_HOMEPAGE } from "../constants/links";
+
+const MyPosts = ({ navigation }) => {
+  const [posts, setPosts] = useState([]);
+  const user = useSelector((state) => state.user.user);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(POST_HOMEPAGE);
+       //console.log('API Response:', response.data); // API yanıtını konsola loglayın
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching posts:', error); // Hata durumunu loglayın
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      
+      
+
+      <ScrollView>
+        {posts.map((post) => (
+          <Card key={post._id} style={styles.card}>
+            <Card.Title
+              title={post.title}
+              subtitle={post.routeType}
+            />
+            <TouchableOpacity onPress={() => navigation.navigate('PostDetail', { postId: post._id })}>
+              <Card.Cover source={{ uri: post.images[0] }} />
+            </TouchableOpacity>
+            <Card.Content>
+              <Text style={styles.caption}>{post.description}</Text>
+            </Card.Content>
+            <Card.Actions>
+              <PaperButton icon="heart-outline" onPress={() => { }}>Like</PaperButton>
+              <PaperButton icon="comment-outline" onPress={() => { }}>Comment</PaperButton>
+            </Card.Actions>
+          </Card>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f8f9fa",
+  },
+  card: {
+    margin: 10,
+    borderRadius: 8,
+  },
+  caption: {
+    marginTop: 10,
+  },
+  feedContainer: {
+    display: "flex",
+  },
+  logo: {
+    width: 40,
+    height: 32,
+  },
+  logoText: {
+    height: 35,
+    fontSize: 20,
+    top: 3,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+  },
+  logoContainer: {
+    width: "70%",
+    height: 40,
+    backgroundColor: "green",
+    borderRadius: 40,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  postButton: {
+    backgroundColor: "#6200ee",
+    padding: 10,
+    margin: 20,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  postButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
+export default MyPosts;
