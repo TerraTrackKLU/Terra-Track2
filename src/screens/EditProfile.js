5821b569a7561d97ff3a361fc9e0e74b854ff629
augurgaug <ugurgl_65@hotmail.com>
2024-06-28 00:16:53 +0300

    pp ekleme

--- src/screens/EditProfile.js
+++ src/screens/EditProfile.js
@@ -67,7 +67,7 @@ const EditProfile = ({ navigation }) => {
         profilePic,
       };
 
-      console.log("Updated User Data:", updatedUser);
+      //console.log("Updated User Data:", updatedUser);
 
       const response = await axios.put(UPDATE_USER, updatedUser, {
         headers: {
@@ -105,7 +105,16 @@ const EditProfile = ({ navigation }) => {
     });
 
     if (!result.canceled) {
-      setProfilePic(result.assets[0].uri);
+      const imageUri = result.assets[0].uri;
+
+      // Convert the image to base64 format
+      const response = await fetch(imageUri);
+      const blob = await response.blob();
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setProfilePic(reader.result);
+      };
+      reader.readAsDataURL(blob);
     }
   };
 
